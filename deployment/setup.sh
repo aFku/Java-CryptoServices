7afbdd92cd2ccbf880d9f5e4c42a2575e6a28dd7
aFku <zapasowyrowerek@gmail.com>
2023-03-27 12:39:10 -0400

    Added metallb deployment

--- deployment/setup.sh
+++ deployment/setup.sh
@@ -9,16 +9,16 @@ cd Python-3.8.1/
 ./configure --enable-optimizations
 make altinstall
 
-# Install Rust
-#curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs -o /tmp/sh.rustup.rs
-#sh ./sh.rustup.rs
-#rustup update
-#su - user -c "cd ~"
-
 # Install ansible
 su - user -c "python3.8 -m pip install --user --upgrade pip"
 su - user -c "python3.8 -m pip install --user ansible"
 
 # Install ansible module
+su - user -c "python3.8 -m pip install --user kubernetes"
 su - user -c "ansible-galaxy collection install kubernetes.core"
 
+# Start ansible deploy
+su - user -c "ansible-playbook main.yaml --ask-become-pass -vvv"
+su - user -c "newgrp docker"
+su - user -c "ansible-playbook main-2.yaml --ask-become-pass -vvv"
+su - user -c "ansible-playbook main-3.yaml --ask-become-pass -vvv"
