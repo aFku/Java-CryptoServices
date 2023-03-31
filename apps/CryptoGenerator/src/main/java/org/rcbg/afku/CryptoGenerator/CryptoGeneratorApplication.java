74193cc182ca593696051851a1f3d5b679619730
aFku <zapasowyrowerek@gmail.com>
2023-03-31 22:48:16 +0200

    Init password generator

---
+++ apps/CryptoGenerator/src/main/java/org/rcbg/afku/CryptoGenerator/CryptoGeneratorApplication.java
@@ -0,0 +1,13 @@
+package org.rcbg.afku.CryptoGenerator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CryptoGeneratorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CryptoGeneratorApplication.class, args);
+	}
+
+}
